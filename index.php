eac79fe1de71ecac63020c35a4f6d068dc4b0a75
Angela-Reis <aluno20946@ipt.pt>
2023-03-23 19:59:07 +0000

    Add files to connect to the database using PDO

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo "Transmedia"
+require "config/connectdb.php";
+echo "Transmedia";
+
+/*Example of how to execute a SELECT with PDO
+$stmt = $pdo->query('SELECT id, title FROM story');
+$stories = $stmt->fetchAll(PDO::FETCH_ASSOC);*/
 ?>
