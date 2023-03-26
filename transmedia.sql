74bec24cb2bb0ac9f9a8705ef36b8b45c07fcca3
Angela-Reis <aluno20946@ipt.pt>
2023-03-26 15:01:02 +0100

    Add user table to the database

--- transmedia.sql
+++ transmedia.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE IF NOT EXISTS transmedia;
 USE transmedia;
+
+CREATE TABLE user (
+    id INT(11) PRIMARY KEY NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    username VARCHAR(128) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    verified BOOLEAN NOT NULL DEFAULT 0,
+    verificationKey VARCHAR(255) NOT NULL,
+    createDate DATETIME NOT NULL DEFAULT current_timestamp(),
+    updateDate DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
