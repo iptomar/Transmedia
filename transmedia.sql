2edef251cbe9a45e0f40cea9976a47697f63df91
MiguelModesto1 <101589792+MiguelModesto1@users.noreply.github.com>
2023-03-28 02:03:54 +0100

    Update transmedia.sql
    First version of video table

--- transmedia.sql
+++ transmedia.sql
@@ -16,3 +16,23 @@ CREATE TABLE user (
 INSERT INTO user (id, name, email, username, password, verified, verificationKey) VALUES
 (1, 'Joana Silva', 'joana.silva.test@gmail.com', 'joana', '$2y$10$zGvJIpISpzBZgJnF.EUifOTprkIAGdRga9wdVB1JFzf8/hvTJEguO', 1,''),
 (2, 'Tiago Santos', 'tiago.santos.test@gmail.com', 'tiago', '$2y$10$WL7GMQSURQJh/gzfY.Aq7uiQCFcGJPv0bCn7pY15BrEK1G8SdsONC', 1,'');
+
+CREATE TABLE video (
+    id INT(11) PRIMARY KEY NOT NULL,
+    storyId INT(11) NOT NULL,
+    videoTitle VARCHAR(128) NOT NULL,
+    description LONGTEXT NOT NULL,
+    duration INT(11) NOT NULL,
+    publisher VARCHAR(128) NOT NULL,
+    publicationDate DATE NOT NULL,
+    updateDate DATE,
+    videoCode VARCHAR(11) NOT NULL,
+    FOREIGN KEY (storyId) REFERENCES story(Id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO video(id, videoTitle, description, duration, publisher, publicationDate, updateDate, videoCode) VALUES
+(1,
+'Lisp in 100 Seconds', 
+'Lisp is world’s second high-level programming language and is still used to build software today. 
+It was the first to implement many popular programming techniques like higher-order functions, 
+recursion, REPL, garbage collection, and more.', 158, '2022-10-14', 'Fireship', NULL, 'INUHCQST7CU')
