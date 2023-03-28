71a7640e7407a8b67f18b74f6c1cfdfaa5a2e0c4
Hélder Renato <helder_renatoribeiro@hotmail.com>
2023-03-28 14:23:07 +0100

    updating the table and creating the form

--- createStory.php
+++ createStory.php
@@ -7,49 +7,6 @@ if (isset($_POST['submitButton'])) {
     $description = $_POST['description'];
     $author = $_POST['author'];
   
-    //Guarda o url exato 
-    $url = $_SERVER['REQUEST_URI'];
-    //Separa o url pelo username e guarda o username numa variavel
-    $url_components = parse_url($url);
-    parse_str($url_components['query'], $params);
-    $username = $params['username'];
-  
-  
-    //Seleciona o nome da historia da base de dados
-    $video = "SELECT nome FROM historia WHERE nome like '$nomeh'";
-    $result = mysqli_query($conn, $video);
-    $row = mysqli_fetch_row($result);
-  
-    //se o nome da história não existir a história vai ser criada
-    if (empty($row[0])) {
-      // sql query para inserir dados na base de dados
-      $sql_query = "INSERT INTO historia(nome,autor) VALUES('$nomeh','$username')";
-      mysqli_query($conn, $sql_query);
-    }
-  
-  
-  
-    //depois de inserir o nome e duracao da historia é feito um select para identificar o id da historia em questão
-    $id_historia = "SELECT * FROM historia WHERE nome like '$nomeh'";
-    $result = mysqli_query($conn, $id_historia);
-    $row = mysqli_fetch_row($result);
-  
-    //Guarda imagem na pasta /fotos
-    $name = $_FILES["image"]['name'];
-    $tmp_name = $_FILES["image"]['tmp_name'];
-  
-    if (!strpos($name, " ")) {
-      $folder = "../fotos/";
-      if (move_uploaded_file($tmp_name, $folder . $name)) {
-      }
-      //Guarda imagem na base de dados
-      $sql_query = "INSERT INTO imagem(id_historia,imagem,tempo_inicial,tempo_final,autor) VALUES($row[0],'$name','$tempoi','$tempof','$username')";
-      mysqli_query($conn, $sql_query);
-    } else {
-      echo "<script language='javascript'>";
-      echo 'alert("A imagem não pode conter espaços, tente de novo");';
-      echo "</script>";
-    }
   }
 
 ?>
