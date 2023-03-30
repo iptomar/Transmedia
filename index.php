7128f188a6b99719bffc3f25243c80fa2effb82f
Hélder Renato <helder_renatoribeiro@hotmail.com>
2023-03-30 17:08:18 +0100

    landing page

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ echo '<p>Tiago = '.password_hash("tiago123", PASSWORD_DEFAULT).'</p>';*/
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/index.css" type="text/css">
     <title>Transmedia</title>
 </head>
 <body>
@@ -26,5 +27,22 @@ echo '<p>Tiago = '.password_hash("tiago123", PASSWORD_DEFAULT).'</p>';*/
         include "NavBar.php";
         $index = 'index';
     ?>
+    
+    <div class="container">
+        <div class = "logo">
+            <img src="./assets/landing_logo.svg"></img>
+        </div>
+        <div class = "text">
+            TRANS<br>
+            <strong>MEDIA</strong>
+        </div>
+    <div>
+    
+    <div>
+        aaaaaaaaaa
+</div>
+    <div class="availablestories">
+        <a href="#">Available Stories</a>
+    </div>
 </body>
 </html>
