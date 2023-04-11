beed5620174f2d5155ce02e521ec3cf161eca473
Hélder Renato <helder_renatoribeiro@hotmail.com>
2023-04-11 17:38:06 +0100

    landing_page

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ echo '<p>Tiago = '.password_hash("tiago123", PASSWORD_DEFAULT).'</p>';*/
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <link rel="stylesheet" href="./style/index.css" type="text/css">
     <title>Transmedia</title>
 </head>
@@ -28,21 +28,20 @@ echo '<p>Tiago = '.password_hash("tiago123", PASSWORD_DEFAULT).'</p>';*/
         $index = 'index';
     ?>
     
-    <div class="container">
-        <div class = "logo">
-            <img src="./assets/landing_logo.svg"></img>
+    <div>
+        <div class = "content_text">
+            <div class = "logo">
+                <img src="./assets/landing_logo.svg"></img>
+            </div>
+            <div class = "text">
+                TRANS<br>
+                <strong>MEDIA</strong>
+            </div>
         </div>
-        <div class = "text">
-            TRANS<br>
-            <strong>MEDIA</strong>
+
+        <div class="availablestories">
+            <a href="#">Available Stories</a>
         </div>
     <div>
-    
-    <div>
-        aaaaaaaaaa
-</div>
-    <div class="availablestories">
-        <a href="#">Available Stories</a>
-    </div>
 </body>
 </html>
