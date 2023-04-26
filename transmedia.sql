ac3c7ee64897b86e11a297abdc2e64adb2e6c722
Hélder Renato <helder_renatoribeiro@hotmail.com>
2023-04-26 13:54:36 +0100

    Update transmedia.sql

--- transmedia.sql
+++ transmedia.sql
@@ -1,106 +1,137 @@
-CREATE DATABASE IF NOT EXISTS transmedia;
+-- Data Base `transmedia`
+CREATE DATABASE IF NOT EXISTS `transmedia` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
-USE transmedia;
+USE `transmedia`;
 
-CREATE TABLE user (
-    id INT(11) PRIMARY KEY NOT NULL,
-    name VARCHAR(128) NOT NULL,
-    email VARCHAR(128) UNIQUE NOT NULL,
-    username VARCHAR(128) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    verified BOOLEAN NOT NULL DEFAULT 0,
-    verificationKey VARCHAR(255) NOT NULL,
-    createDate DATETIME NOT NULL DEFAULT current_timestamp(),
-    updateDate DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+-- --------------------------------------------------------
+-- Structure of table `user`
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT 0,
+  `verificationKey` varchar(255) NOT NULL,
+  `createDate` datetime NOT NULL DEFAULT current_timestamp(),
+  `updateDate` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-INSERT INTO
-    user (
-        id,
-        name,
-        email,
-        username,
-        password,
-        verified,
-        verificationKey
-    )
-VALUES
-    (
-        1,
-        'Joana Silva',
-        'joana.silva.test@gmail.com',
-        'joana',
-        '$2y$10$zGvJIpISpzBZgJnF.EUifOTprkIAGdRga9wdVB1JFzf8/hvTJEguO',
-        1,
-        ''
-    ),
-    (
-        2,
-        'Tiago Santos',
-        'tiago.santos.test@gmail.com',
-        'tiago',
-        '$2y$10$WL7GMQSURQJh/gzfY.Aq7uiQCFcGJPv0bCn7pY15BrEK1G8SdsONC',
-        1,
-        ''
-    );
-
---
--- Creating the Story Table
---
-CREATE TABLE `Story` (
-    `id` int(11) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `author` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
-
---
--- Degining the Primary key for the Story Table
---
 ALTER TABLE
-    `Story`
+  `user`
 ADD
-    PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT of the table Story
---
-ALTER TABLE
-    `Story`
+  PRIMARY KEY (`id`),
+ADD
+  UNIQUE KEY `email` (`email`),
+ADD
+  UNIQUE KEY `username` (`username`),
 MODIFY
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 231;
+  `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Inserting default data to the Story table
---
+-- Data of table `user`
 INSERT INTO
-    `Story` (`id`, `name`, `description`, `author`)
+  `user` (
+    `id`,
+    `name`,
+    `email`,
+    `username`,
+    `password`,
+    `verified`,
+    `verificationKey`,
+    `createDate`,
+    `updateDate`
+  )
 VALUES
-    (1, 'lol', 'jogar lol', 'filipeguia'),
-    (4, 'Saude', 'Levar vacina', ''),
-    (84, 'Teste2', '', 'filipeguia'),
-    (224, 'Sporting', '', 'filipeguia'),
-    (228, 'Benfica', '', 'filipeguia'),
-    (229, 'Porto', '', 'filipeguia'),
-    (230, 'Desporto', '', 'filipeguia');
+  (
+    1,
+    'Joana Silva',
+    'joana.silva.test@gmail.com',
+    'joana',
+    '$2y$10$zGvJIpISpzBZgJnF.EUifOTprkIAGdRga9wdVB1JFzf8/hvTJEguO',
+    1,
+    '',
+    '2023-04-12 17:36:31',
+    '2023-04-12 17:36:31'
+  ),
+  (
+    2,
+    'Tiago Santos',
+    'tiago.santos.test@gmail.com',
+    'tiago',
+    '$2y$10$WL7GMQSURQJh/gzfY.Aq7uiQCFcGJPv0bCn7pY15BrEK1G8SdsONC',
+    1,
+    '',
+    '2023-04-12 17:36:31',
+    '2023-04-12 17:36:31'
+  );
 
-CREATE TABLE video (
-    id INT(11) PRIMARY KEY NOT NULL,
-    storyId INT(11) NOT NULL,
-    videoTitle VARCHAR(128) NOT NULL,
-    description LONGTEXT NOT NULL,
-    duration INT(11) NOT NULL, -- duration in seconds
-    publisher VARCHAR(128) NOT NULL,
-    publicationDate DATE NOT NULL,
-    videoCode VARCHAR(11) NOT NULL,
-    FOREIGN KEY (storyId) REFERENCES story(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- --------------------------------------------------------
+-- Structure of table `story`
+CREATE TABLE `story` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-INSERT INTO video(id, storyId, videoTitle, description, duration, publisher, publicationDate, videoCode) VALUES
-(1, 84,
-'Lisp in 100 Seconds', 
-'Lisp is world’s second high-level programming language and is still used to build software today. 
-It was the first to implement many popular programming techniques like higher-order functions, 
-recursion, REPL, garbage collection, and more.', 158, 'Fireship', '2022-10-14', 'INUHCQST7CU')
+ALTER TABLE
+  `story`
+ADD
+  PRIMARY KEY (`id`),
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+ADD
+  FOREIGN KEY (`author`) REFERENCES `user` (`username`);
 
+-- Data of table `story`
+INSERT INTO
+  `story` (`id`, `name`, `description`, `author`)
+VALUES
+  (1, 'lol', 'jogar lol', 'joana'),
+  (4, 'Saude', 'Levar vacina', 'tiago'),
+  (84, 'Teste2', '', 'tiago'),
+  (224, 'Sporting', '', 'joana'),
+  (228, 'Benfica', '', 'tiago'),
+  (229, 'Porto', '', 'joana'),
+  (230, 'Desporto', '', 'tiago'),
+  (231, 'teste', 'lorem ipsum', 'joana');
+
+-- --------------------------------------------------------
+-- Structure of table `video`
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL,
+  `storyId` int(11) NOT NULL,
+  `storyOrder` int(11) NOT NULL,
+  `link` varchar(128) NOT NULL,
+  `duration` int(11) NOT NULL,
+  `videoType` varchar(128) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+ALTER TABLE
+  `video`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  UNIQUE KEY `storyOrder` (`storyId`, `storyOrder`),
+ADD
+  KEY `storyId` (`storyId`),
+MODIFY
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+ADD
+  CONSTRAINT `video_ibfk_1` FOREIGN KEY (`storyId`) REFERENCES `story` (`id`);
+
+INSERT INTO
+  `video` (
+    `id`,
+    `storyId`,
+    `storyOrder`,
+    `link`,
+    `duration`,
+    `videoType`
+  )
+VALUES
+  (1, 228, 1, 'video_1682034312.mp4', 107, 'file'),
+  (2, 224, 1, 'CKThDImMq3o', 103, 'text'),
+  (3, 228, 2, 'HihzFAZ1XbA', 415, 'text'),
+  (4, 1, 1, 'FGlhWPwrkDg', 196, 'text'),
+  (5, 1, 2, 'mDYqT0_9VR4', 242, 'text');
