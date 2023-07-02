42b075b765aa57b621679c7d172e54fb8fc963d2
Angela-Reis <aluno20946@ipt.pt>
2023-07-02 23:37:49 +0100

    Update content of story and media tables to align with project context

--- transmedia.sql
+++ transmedia.sql
@@ -83,19 +83,6 @@ MODIFY
 ADD
   FOREIGN KEY (`author`) REFERENCES `user` (`username`);
 
--- Data of table `story`
-INSERT INTO
-  `story` (`id`, `name`, `description`, `author`)
-VALUES
-  (1, 'lol', 'jogar lol', 'joana'),
-  (4, 'Saude', 'Levar vacina', 'tiago'),
-  (84, 'Teste2', '', 'tiago'),
-  (224, 'Sporting', '', 'joana'),
-  (228, 'Benfica', '', 'tiago'),
-  (229, 'Porto', '', 'joana'),
-  (230, 'Desporto', '', 'tiago'),
-  (231, 'teste', 'lorem ipsum', 'joana'),
-  (232, 'teste3', 'lorem', 'joana');
 
 -- --------------------------------------------------------
 -- Structure of table `video`
@@ -121,22 +108,6 @@ MODIFY
 ADD
   CONSTRAINT `video_ibfk_1` FOREIGN KEY (`storyId`) REFERENCES `story` (`id`);
 
-INSERT INTO
-  `video` (
-    `id`,
-    `storyId`,
-    `storyOrder`,
-    `link`,
-    `duration`,
-    `videoType`
-  )
-VALUES
-  (1, 228, 1, 'video_1682034312.mp4', 107, 'file'),
-  (2, 224, 1, 'CKThDImMq3o', 103, 'text'),
-  (3, 228, 2, 'HihzFAZ1XbA', 415, 'text'),
-  (4, 1, 1, 'FGlhWPwrkDg', 196, 'text'),
-  (5, 1, 2, 'mDYqT0_9VR4', 242, 'text'),
-  (6, 232, 1, 'video_1683714500.mp4', 24, 'file');
 
 -- Create Audio Table
 CREATE TABLE `audio` (
@@ -165,23 +136,6 @@ MODIFY
 ADD
   UNIQUE KEY `storyOrder` (`id_story`, `storyOrder`);
 
-INSERT INTO
-  `audio` (
-    `id`,
-    `id_story`,
-    `audio`,
-    `duration`,
-    `storyOrder`
-  )
-VALUES
-  (1, 228, 'audio_1683677229.mp3', 107, 1),
-  (2, 232, 'audio_1683714672.mp3', 68, 2),
-  (3, 232, 'audio_1683714865.mp3', 151, 8),
-  (4, 232, 'audio_1683714875.mp3', 1, 4),
-  (5, 232, 'audio_1683714886.mp3', 1, 3),
-  (6, 232, 'audio_1683714893.mp3', 1, 5),
-  (7, 232, 'audio_1683714984.mp3', 2, 7),
-  (8, 232, 'audio_1683714997.mp3', 2, 6);
 
 -- Alter the table to add ON DELETE CASCADE to foreign key constraint
 ALTER TABLE
@@ -222,6 +176,7 @@ CREATE TABLE `image` (
   FOREIGN KEY (`storyID`) REFERENCES `story` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
+
 -- Structure of the table Text-----------------
 CREATE TABLE `text` (
   `id` int(11) NOT NULL,
@@ -232,82 +187,6 @@ CREATE TABLE `text` (
   `author` varchar(255) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-INSERT INTO
-  `text` (
-    `id`,
-    `id_story`,
-    `duration`,
-    `storyorder`,
-    `text`,
-    `author`
-  )
-VALUES
-  (1, 1, '4', '1', 'ola', 'manuel'),
-  (24, 1, '1', '2', 'qwerty', 'filipeguia'),
-  (
-    27,
-    224,
-    '0',
-    '1',
-    'O mundo sabe que pelo teu amor eu sou doente',
-    'filipeguia'
-  ),
-  (
-    28,
-    224,
-    '0,13',
-    '2',
-    'Farei o meu melhor para te ver sempre na frente',
-    'filipeguia'
-  ),
-  (
-    29,
-    224,
-    '0,26',
-    '3',
-    'Irei onde o coração me levar',
-    'filipeguia'
-  ),
-  (
-    30,
-    224,
-    '0,36',
-    '4',
-    'E sem receio farei o que puder pelo meu Sporting',
-    'filipeguia'
-  ),
-  (
-    31,
-    224,
-    '0,52',
-    '5',
-    'E todo mundo sabe que ',
-    'filipeguia'
-  ),
-  (
-    32,
-    224,
-    '0,59',
-    '6',
-    'Pelo teu amor eu sou doente',
-    'filipeguia'
-  ),
-  (
-    33,
-    224,
-    '1,06',
-    '7',
-    'Então farei o meu melhor para te ver sempre na frente',
-    'filipeguia'
-  ),
-  (
-    34,
-    224,
-    '1,19',
-    '8',
-    'E eu farei o que puder pelo meu Sportinggg!',
-    'filipeguia'
-  );
 
 ALTER TABLE
   `text`
@@ -327,11 +206,92 @@ MODIFY
   `id` int(11) NOT NULL AUTO_INCREMENT,
   AUTO_INCREMENT = 36;
 
-ALTER TABLE `story`
-DROP FOREIGN KEY `story_ibfk_1`;
+ALTER TABLE
+  `story` DROP FOREIGN KEY `story_ibfk_1`;
 
 
 ALTER TABLE
   `story`
 ADD
   CONSTRAINT `story_ibfk_1` FOREIGN KEY (`author`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+DELETE FROM
+  `story`;
+
+ALTER TABLE
+  `story` AUTO_INCREMENT = 1;
+
+DELETE FROM
+  `video`;
+
+ALTER TABLE
+  `video` AUTO_INCREMENT = 1;
+
+DELETE FROM
+  `audio`;
+
+ALTER TABLE
+  `audio` AUTO_INCREMENT = 1;
+
+DELETE FROM
+  `image`;
+
+ALTER TABLE
+  `image` AUTO_INCREMENT = 1;
+
+DELETE FROM
+  `text`;
+
+ALTER TABLE
+  `text` AUTO_INCREMENT = 1;
+
+
+INSERT INTO `story` (`id`, `name`, `description`, `author`) VALUES
+(1, 'Festa dos Tabuleiros', 'Única no mundo, a Festa dos Tabuleiros realiza-se de quatro em quatro anos. ', 'tiago'),
+(2, 'Convento de Cristo', '', 'joana'),
+(3, 'Aqueduto dos Pegões Alto', '', 'tiago'),
+(4, 'Sinagoga de Tomar', '', 'tiago'),
+(5, 'Igreja de Santa Maria dos Olivais', '', 'tiago'),
+(6, 'Estátua de D. Gualdim Pais', '', 'joana'),
+(7, 'Mata Nacional dos Sete Montes', '', 'joana');
+
+INSERT INTO `video` (`id`, `storyId`, `storyOrder`, `link`, `duration`, `videoType`) VALUES
+(1, 1, 2, '5-rVvOse7Kw', 242, 'text'),
+(2, 1, 1, 'qZ884ArWlNw', 144, 'text'),
+(4, 2, 2, 'video_1688334536.mp4', 161, 'file'),
+(5, 2, 1, 'L343mYl7GNY', 60, 'text'),
+(6, 5, 1, 'J93e9QskKM4', 154, 'text'),
+(7, 7, 1, 'Bgo67_sIhgU', 309, 'text');
+
+INSERT INTO `audio` (`id`, `id_story`, `audio`, `duration`, `storyOrder`) VALUES
+(1, 1, 'audio_1688332678.mp3', 241, 2),
+(2, 1, 'audio_1688333851.mp3', 143, 1),
+(3, 3, 'audio_1688335000.mp3', 62, 1),
+(4, 7, 'audio_1688336140.mp3', 309, 1);
+
+INSERT INTO `image` (`id`, `storyID`, `image`, `duration`, `storyOrder`) VALUES
+(3, 1, 'image_1688332828.jpg', 71, 8),
+(4, 1, 'image_1688332844.jpg', 75, 3),
+(5, 1, 'image_1688332938.jpg', 40, 1),
+(6, 1, 'image_1688333047.webp', 40, 2),
+(8, 1, 'image_1688333384.jpg', 40, 6),
+(9, 1, 'image_1688333449.webp', 40, 4),
+(10, 1, 'image_1688333685.jpg', 40, 7),
+(12, 2, 'image_1688334478.jpg', 100, 2),
+(14, 2, 'image_1688334632.webp', 80, 3),
+(18, 3, 'image_1688335060.webp', 20, 2),
+(19, 4, 'image_1688335332.jpg', 100, 1),
+(20, 3, 'image_1688335712.jpg', 20, 1),
+(21, 6, 'image_1688335925.jpg', 300, 1),
+(22, 7, 'image_1688336106.jpg', 50, 1),
+(23, 7, 'image_1688336119.jpg', 40, 2),
+(24, 7, 'image_1688336169.webp', 40, 3),
+(26, 2, 'image_1688336526.jpg', 60, 1),
+(27, 2, 'image_1688336599.webp', 60, 4),
+(28, 1, 'image_1688336722.webp', 40, 5);
+
+INSERT INTO `text` (`id`, `id_story`, `duration`, `storyorder`, `text`, `author`) VALUES
+(1, 1, '144', '1', 'Os melhores momentos das Saídas de Coroas da Festa dos Tabuleiros 2023', 'tiago'),
+(2, 1, '242', '2', 'Festa de Tabuleiros Tomar', 'tiago'),
+(3, 4, '10', '1', 'Raríssimo exemplar dos templos judaicos medievais e da arte pré-renascentista portuguesa', 'tiago'),
+(4, 4, '20', '2', 'Sinagoga de Tomar é a única, dessa época, integralmente conservada ainda existente em Portugal. ', 'tiago');
