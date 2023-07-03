aef6d302b47ae6cc125eed224d7d2e20238e91a6
Angela-Reis <aluno20946@ipt.pt>
2023-07-03 12:43:26 +0100

    Fix images on the selected story page so they're responsive

--- selectedStoryPage.php
+++ selectedStoryPage.php
@@ -34,7 +34,7 @@ $totaltimeText = array_sum(array_column($textFetch, 'duration'));
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="./style/selected_story_page.css" type="text/css">
+    <link rel="stylesheet" href="./style/selected_story_page.css" type="text/css">
     <title>Story Selected</title>
 </head>
 
